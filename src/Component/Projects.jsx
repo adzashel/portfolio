da81dfed6b2d927c5efbe69c50740e8b56a7d50e
adzashel <seigneur.noir15@gmai.com>
2025-01-06 11:03:28 +0700

    minor update

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -3,9 +3,113 @@ import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 import { ProjectContainer } from "../Component/ProjectContainer";
-import project1 from '../database/project1.json'
-import project2 from '../database/project2.json'
-import project3 from '../database/project3.json'
+import projImg1 from "../assets/img/books.png";
+import projImg2 from "../assets/img/Calculator.png";
+import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/MongoDb.png";
+import projImg5 from "../assets/img/project-img1.png";
+import projImg6 from "../assets/img/webport.png";
+import projImg7 from "../assets/img/todolist.png";
+import projImg8 from "../assets/img/WebDesign.png";
+import projImg9 from "../assets/img/GitHub-search.png";
+import projectImg10 from "../assets/img/quote-generator.png";
+import project11 from "../assets/img/BookApp.png";
+import project12 from "../assets/img/weatherApp.png";
+import project13 from "../assets/img/projectKhodam.png"
+import project14 from '../assets/img/express_login.png';
+import project15 from '../assets/img/coffeeshop.png'
+
+
+const project1 = [
+  {
+    title: "Books Directory",
+    description: "Vite And Bootstrap",
+    imgUrl: projImg1,
+  },
+  {
+    title: "Calculator",
+    description: "HTML , CSS, JavaScript",
+    imgUrl: projImg2,
+  },
+  {
+    title: "Employee ContactApp",
+    description: "ExpressJS , JavaScript, NodeJs, Bootstrap",
+    imgUrl: projImg4,
+  },
+  {
+    title: "Business Startup",
+    description: "ReactJs",
+    imgUrl: projImg3,
+  },
+  {
+    title: "Travel Landing Page",
+    description: "Vuejs ",
+    imgUrl: projImg5,
+  },
+  {
+    title: "Portfolio Website",
+    description: "Vite , bootstrap, ReactJs",
+    imgUrl: projImg6,
+  },
+]
+
+const project2 = [
+  {
+    title: "TodoList App ",
+    desc: "Html , CSS , JavaScript",
+    image: projImg7,
+    link: "https://github.com/adzashel",
+  },
+  {
+    title: "Github Search User ",
+    desc: "React js",
+    image: projImg9,
+    link: "https://github-user-pink.vercel.app/",
+  },
+  {
+    title: "Web Design ",
+    desc: "Figma , Photoshop",
+    image: projImg8,
+    link: "https://www.youtube.com/@WindahBasudara/streams",
+  },
+  {
+    title: "Quote Generator ",
+    desc: "ReactJs , NodeJs , JavaScript",
+    image: projectImg10,
+    link: "https://random-quotes-sigma-sooty.vercel.app/",
+  },
+  {
+    title: "Book Application ",
+    desc: "ReactJs , NodeJs , JavaScript",
+    image: project11,
+    link: "https://bookapp-sepia.vercel.app/",
+  },
+  {
+    title: " Weather Application ",
+    desc: "ReactJs , NodeJs , JavaScript",
+    image: project12,
+    link: "https://weather-app-mu-sooty-55.vercel.app/",
+  },
+]
+
+const project3 = [
+  {
+    title: "Cek Khodam App",
+    desc: "Reactjs",
+    image: project13,
+    link: "https://cek-khodam-roan.vercel.app/",
+  },
+  {
+    title : "Login Form",
+    desc : "Expressjs , MongoDB",
+    image : project14,
+  },
+  {
+    title : "Coffee Shop",
+    image : project15,
+    desc : "Reactjs",
+  }
+];
 
 export const Projects = () => {
  
